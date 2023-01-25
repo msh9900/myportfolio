6d75beb53039eda21f6f2a5816764e935f631122
MOS <msh9900@naver.com>
2023-01-25 16:39:25 +0900

    포트폴리오 수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+        <h1 className="title-font sm:text-5xl text-4xl mb-4 font-medium text-gray-900">
           안녕하세요 MOS 입니다.
         </h1>
         <p className="mb-8 leading-relaxed">
